0512d752ff7c68408288b722f8d42ffbbf0f9ea7
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2020-12-09 01:04:25 +0200

    ДЗ 3

--- js/task-number_6.js
+++ js/task-number_6.js
@@ -1,7 +1,38 @@
 "use strict"
 
-let input;
-const numbers = [];
-let total = 0;
 
-prompt('Введи число');
+let inputArray = [];
+const askUser = function () {
+  let input = prompt("Че как по числам?");
+  if (input === null) {
+    let summ = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+      summ = summ + inputArray[i];
+    }
+    console.log(`Сумма чисел ${summ}`);
+    return;
+  }
+  inputArray.push(Number(input));
+  askUser();
+};
+
+askUser();
+
+
+
+
+let inputArray = [];
+let isComplete = false;
+while (!isComplete) {
+  let input = prompt("Че как по числам?");
+  if (input === null) {
+    let summ = 0;
+    for (let item of inputArray) {
+      summ = summ + item;
+    }
+    console.log(`Сумма чисел ${summ}`);
+    isComplete = true;
+  }
+  inputArray.push(Number(input));
+}
+
